621d7c60ab3889cd208e51f55a37ff7175fc6a1f
DianaMayesCloud <diana.lmayes@gmail.com>
2020-11-03 17:46:51 -0500

    backend slash deleted

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 0.12.0"
   backend "s3" {
     bucket = "statefile.bucket.ohio"
-    key    = "/jenkins/terraform.tfstate"
+    key    = "jenkins/terraform.tfstate"
     region = "us-east-2"
   }
 }
